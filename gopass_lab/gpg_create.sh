a135c7906bd14b0ff1d8977bb01ab19a25d1407a
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2022-10-07 11:10:55 -0300

    Create gpg_create.sh

---
+++ gopass_lab/gpg_create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat >foo <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: RSA
+     Key-Length: 1024
+     Subkey-Type: RSA
+     Subkey-Length: 1024
+     Name-Real: $USER Tester
+     Name-Email: $USER@foo.bar
+     Expire-Date: 0
+     Passphrase: abc123
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+
+gpg --batch --generate-key foo
